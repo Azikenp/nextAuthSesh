e1f82940a18c093ea402317fc58bd9ee99011829
Azikenp <azikenp26@gmail.com>
2025-04-08 10:30:28 +0100

    add NextAuth configuration and remove empty route file

---
+++ app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,9 @@
+import { NextAuthOptions } from "next-auth";
+
+const options: NextAuthOptions = {
+  providers: [],
+  //   pages: {
+  //     signIn: "/auth/signin",
+  //     signOut: "/auth/signout",
+  //   },
+};

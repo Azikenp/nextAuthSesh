efa0a81471e75cf542c80340fc349cf3b6669be9
Azikenp <azikenp26@gmail.com>
2025-04-08 10:42:36 +0100

    add GitHub and Credentials providers to NextAuth configuration

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -1,9 +1,41 @@
 import { NextAuthOptions } from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 const options: NextAuthOptions = {
-  providers: [],
-  //   pages: {
-  //     signIn: "/auth/signin",
-  //     signOut: "/auth/signout",
-  //   },
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: {
+          label: "Username",
+          type: "text",
+          placeholder: "Your username",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "Your password",
+        },
+      },
+      async authorize(credentials) {
+        // Here you can add your own logic to validate the credentials
+        // For example, you can check against a database or an API
+        const user = { id: "20", name: "Aziken", password: "nextaut" };
+
+        if (
+          credentials?.username === user.name &&
+          credentials?.password === user.password
+        ) {
+          return user;
+        } else {
+          return null;
+        }
+      },
+    }),
+  ],
 };

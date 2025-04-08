77e9dd5894f89c55b764205b209fd78084da2dbe
Azikenp <azikenp26@gmail.com>
2025-04-08 10:43:58 +0100

    export NextAuth options and refactor handler initialization

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -2,7 +2,7 @@ import { NextAuthOptions } from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
 import CredentialsProvider from "next-auth/providers/credentials";
 
-const options: NextAuthOptions = {
+export const options: NextAuthOptions = {
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_ID as string,

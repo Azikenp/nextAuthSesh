03a46072645937c553351de47014fef3d8147b18
Azikenp <azikenp26@gmail.com>
2025-04-08 11:02:11 +0100

    add Navbar component, UserCard component, and integrate server session in page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import { getServerSession } from "next-auth";
+import { options } from "./api/auth/[...nextauth]/options";
+import UserCard from "@/components/UserCard";
 
-const page = () => {
+const page = async () => {
+  const session = await getServerSession(options);
   return (
-    <div className='uppercase'>page</div>
-  )
-}
+    <>
+      {session ? (
+        <UserCard user={session?.user} pageType={"Home"} />
+      ) : (
+        <h1 className="text-5xls">You shall not pass</h1>
+      )}
+    </>
+  );
+};
 
-export default page
+export default page;
